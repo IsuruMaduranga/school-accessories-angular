b5405cdd2a216772d2331bc20f790cf509ad8f06
Isuru <isurumaduranga.16@cse.mrt.ac.lk>
2021-06-14 05:25:05 +0530

    Finalize navbar implementation

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  user:boolean = false;
+  admin:boolean = false;
+  none:boolean = true;
+  name:String;
+
+  constructor(private userService: UserService,private router:Router) { }
 
   ngOnInit(): void {
+    this.name=this.userService.name;
+    if(this.userService.type=="USER"){
+      this.user=true;
+      this.none=false;
+    }else if(this.userService.type=="ADMIN"){
+      this.admin=true;
+      this.none=false;
+    }
   }
 
+  logout(){
+    localStorage.removeItem("token");
+
+    Swal.fire({
+      title: 'You are logged out',
+      icon: 'warning',
+      confirmButtonText: 'OK',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.router.navigate(['/login']);
+      } 
+    })
+  }
 }
