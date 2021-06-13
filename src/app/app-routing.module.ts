d2731fd0bf0b8e299f68cfe8a66d9dc06df4d55c
Isuri Thilanka <thilankakh@gmail.com>
2021-06-13 22:37:37 +0530

    add comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,9 @@ import { AdminDataTableComponent } from './admin-analysis/admin-data-table/admin
 import { LoginComponent } from './core/components/login/login.component';
 import { CartCheckoutComponent } from './shopping/cart-checkout/cart-checkout.component';
 import { ShoppingFormComponent } from './shopping/shopping-form/shopping-form.component';
-import { MyOrdersComponent } from "./customer/my-orders/my-orders.component";
-import { CustomerAnalyticsService } from "./customer/services/customer-analytics.service";
-import { CustomerAnalyticsComponent } from "./customer/customer-analytics/customer-analytics.component";
+import { MyOrdersComponent } from './customer/my-orders/my-orders.component';
+import { CustomerAnalyticsService } from './customer/services/customer-analytics.service';
+import { CustomerAnalyticsComponent } from './customer/customer-analytics/customer-analytics.component';
 import { ProductsComponent } from './shopping/products/products.component';
 import { RegisterComponent } from './core/components/register/register.component';
 import { UsersComponent } from './core/components/users/users.component';
@@ -18,17 +18,17 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'users', component: UsersComponent },
-  { path: 'products', component: ProductsComponent },
-  { path: 'purchase', component: ShoppingFormComponent },
-  { path: 'cart-checkout', component: CartCheckoutComponent },
-  { path: 'admin-data-table', component: AdminDataTableComponent },
-  { path: 'admin-charts', component: AdminChartsComponent },
-  { path: 'my-orders', component: MyOrdersComponent },
-  { path: 'customer-analytics', component: CustomerAnalyticsComponent }
+  { path: 'products', component: ProductsComponent }, //both
+  { path: 'purchase', component: ShoppingFormComponent }, //user
+  { path: 'cart-checkout', component: CartCheckoutComponent }, //user
+  { path: 'admin-data-table', component: AdminDataTableComponent }, //admin
+  { path: 'admin-charts', component: AdminChartsComponent }, //admin
+  { path: 'my-orders', component: MyOrdersComponent }, //user
+  { path: 'customer-analytics', component: CustomerAnalyticsComponent }, //customer
 ];
 
 @NgModule({
   imports: [FormsModule, RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
