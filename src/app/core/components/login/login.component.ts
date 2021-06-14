345783e3d5050211964f68834748005a7facf368
Isuri Thilanka <thilankakh@gmail.com>
2021-06-14 11:13:37 +0530

    change alignment in nav bar elements

--- src/app/core/components/login/login.component.ts
+++ src/app/core/components/login/login.component.ts
@@ -1,29 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { LoginRequest } from 'src/app/shared/models/LoginRequest';
 import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
 
-  constructor(private userService: UserService) { 
-  }
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
-      email: new FormControl(null, [
-        Validators.required,
-        Validators.email
-      ]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [
         Validators.required,
-        Validators.minLength(6)
-      ])
+        Validators.minLength(6),
+      ]),
     });
   }
 
@@ -33,21 +30,23 @@ export class LoginComponent implements OnInit {
       this.loginForm.value.password
     );
 
-    this.userService.login(loginReq).subscribe(res => {
-      if (res.token) {
-        localStorage.setItem('token', res.token);
-      } else {
-        alert("Server Error!");
+    this.userService.login(loginReq).subscribe(
+      (res) => {
+        if (res.token) {
+          localStorage.setItem('token', res.token);
+          this.router.navigate(['products']);
+        } else {
+          alert('Server Error!');
+        }
+      },
+      (e) => {
+        if (e.error instanceof ProgressEvent) {
+          console.log(e);
+          alert('An error occurred!');
+        } else {
+          alert(e.error.message);
+        }
       }
-    }, e => {
-      if (e.error instanceof ProgressEvent) {
-        console.log(e);
-        alert('An error occurred!');
-      } else {
-        alert(e.error.message);
-      }
-    });
+    );
   }
-
-
 }
